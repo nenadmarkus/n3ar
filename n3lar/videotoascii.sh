5fa6992aa4fa183bd24fac40740a2291022438b8
nenad <neno.markus@gmail.com>
2014-03-19 17:41:40 +0100

    added video to ASCII script

---
+++ n3lar/videotoascii.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#
+#
+#
+
+#
+# temporary directory
+#
+
+mkdir TMP
+
+#
+# decompose a video into individual frames
+#
+
+ffmpeg -i $1 TMP/%06d.jpg
+
+#
+# transform frames to ASCII art
+#
+
+COUNTER=0
+
+for frame in TMP/*.jpg
+do
+	#
+	printf "$frame\n"
+
+	#
+	./n3lar $frame $frame.ascii.png
+
+	#
+	let COUNTER=COUNTER+1
+done
+
+#
+# stitch the individual frames to a video
+#
+
+ffmpeg -i TMP/%06d.jpg.ascii.png $2
+#ffmpeg -r $2 -i TMP/%06d.jpg.ascii.png $3
+
+#
+# clean temporary files
+#
+
+rm TMP/*.jpg
+rm TMP/*.ascii.png
+
+rmdir TMP
